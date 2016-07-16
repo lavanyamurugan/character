f7042546b27e77a6ec8e01ebff52819a34987e00
lavanyamurugan <lavanyamkala@gmail.com>
2016-07-16 15:30:20 +0530

    Create char.c
    finish

---
+++ char.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+void main(0
+{
+char a[]="lavanya";
+int i;
+for(i=0;i<10;i++)
+if((a[i]=='a')||a[i]=='e')||a[i]=='i')||a[i]=='o')||a[i]=='u'))
+{
+printf("the char is vowel%s",i);
+}
